fca0a441ab9c7bc91d658559940499dac9798c46
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-01 18:19:52 +0500

    feat: add initial commit
    created the realtime-tracker app using express.js, socket.io, ejs &
    leaflet API

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function (socket) {
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", function () {
+    io.emit("user-disconnect", socket.id);
+  });
+});
+
+app.get("/", function (_, res) {
+  res.render("index");
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
+});
